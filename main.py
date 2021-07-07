d5796c5f63a3ddc251649cd984a4ad6bfb2fa9ed
toni <toni@pop-os.localdomain>
2021-07-07 20:32:27 +0200

    Finished everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from json import dump
+from typing import ChainMap
 from discord.ext.commands.core import check
 import tools
 import discord
@@ -16,44 +17,57 @@ client = discord.Client(intents=intents)
 db = DB("db/addrtouser.db")
 db1 = DB("db/addrstorage.db")
 db2 = DB("db/number.db")
+db3 = DB("db/calluser.db")
 
 client = commands.Bot(command_prefix = "!")
-async def test1(): 
 
-    checkaddr = db1.geta()#  second addreses
-    print(db1.geta())
-    checkaddr1 = list(checkaddr.values())
-    print(checkaddr1)
-    for element in checkaddr1:
-        print('check' + element)
-        json = await tools.getonl(element)
-        if json == False:
-            author = int(db.get(element))
-            owner = await client.fetch_user(author)  # your user ID
-            await owner.send("YOUR NODE IS NOT ONLINE!")    
 async def check(): 
+
     while True:
         checkaddr = db1.geta()#  second addreses
         print(db1.geta())
         checkaddr1 = list(checkaddr.values())
-        print(checkaddr1)
+        print('All addresses users:',checkaddr1)
+        checkuser = db3.geta()#  second addreses
+        print(db3.geta())
+        checkuserr1 = list(checkuser.values())
+        print('All called users:',checkuserr1)
+
+        how = db2.get("how")
+
         for element in checkaddr1:
             print('check' + element)
             json = await tools.getonl(element)
             if json == False:
+                print(element+',is ofline')
+
                 author = int(db.get(element))
                 owner = await client.fetch_user(author)  # your user ID
-                if len(client.cached_messages):
-                    await owner.send("YOUR NODE IS NOT ONLINE!")   
+                if author not in checkuserr1:
+                    await owner.send("YOUR NODE IS NOT ONLINE!,"+element)
+                    db3.set(how,author) #Sets Value
+   
 
-        await asyncio.sleep(600)
+        await asyncio.sleep(900)
 
+async def check1(): 
+    while True:
+        print('all users delited from calluser.db')
+        db3.resetdb()#  second addreses
+        await asyncio.sleep(3600)
 
+async def main():
+    while True:
+        await check()
+        await check1()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     await client.change_presence(activity=discord.Game(name="`!cmds` for help!"))
-    await check()
+    loop = asyncio.get_event_loop()
+    loop.create_task(check())
+    loop.create_task(check1())
+
 
 @client.command()
 async def watch(ctx, arg):
@@ -69,6 +83,7 @@ async def watch(ctx, arg):
             await ctx.send('Remove number:'+ str(number))
 
             db.set(arg , author) #Sets Value
+
             db1.set(number,arg) #Sets Value
             db2.set('how',number) #Sets Value
 
@@ -98,12 +113,20 @@ async def reset(ctx):
         db.resetdb()
         db1.resetdb()
         db2.resetdb()
+        db3.resetdb()
+
     else:
         await ctx.send('INVALID COMMAND')
 @client.command()
 async def test(ctx):
-      await test1()
-
+    author = str(ctx.message.author.id)   
+    how = db2.get("how")
+     
+    db3.set(how,author) #Sets Value
+@client.command()
+async def test1(ctx):
+    await check1()
+     
 @client.command()
 async def cmds(ctx):
     await ctx.send('---Help--')
@@ -116,4 +139,4 @@ async def cmds(ctx):
  
 
 
-client.run('ODYxOTAyMDA3NjM0NDkzNDUw.YOQiyw.YN8sxlLQvZL-SVuyFvxlKDXEz7o')
+client.run('ODYxOTAyMDA3NjM0NDkzNDUw.YOQiyw.ZY2-lCUQ6g4F8xj4vj0gRtUWgiA')
